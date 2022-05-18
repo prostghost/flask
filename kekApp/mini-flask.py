9d7f20888ee1821f221a4f3e82278b9f2a4bc5f7
prostghost <glyantsev27@gmail.com>
2022-05-19 00:41:54 +0300

    dadaya

--- kekApp/mini-flask.py
+++ kekApp/mini-flask.py
@@ -1,13 +1,30 @@
+from re import template
 from urllib import response
-from flask import Flask, render_template, url_for, request
+from flask import Flask, jsonify, render_template, url_for, request
 import requests
-from flasgger import Swagger
+from flasgger import Swagger, swag_from
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
-swagger = Swagger(api)
 
+#template для swagger'a
+template = {
+    "swagger":"2.0",
+    "info":{
+        "title":"API",
+        "description":"API for webapp",
+        "contact":{
+            "email":"example@qq.com",
+            "url":"aboba.com"
+        },
+        "version":"0.0.1"
+    },
+}
+
+swagger = Swagger(app, template=template)
+
+#Публичные методы для вызова через внешний API
 methods = {
             # public methods
             'ping':             {'url':'api/v1/ping', 'method': 'GET', 'private': False},
@@ -23,42 +40,61 @@ methods = {
             'tickerBookTicker': {'url': 'api/v3/ticker/bookTicker', 'method': 'GET', 'private': False},
             }
 
+#Библиотека для get запросов
 todos = {
     'todo1': {'task': 'build an API'},
     'todo2': {'task': '?????'},
     'todo3': {'task': 'profit!'},
 }
 
+#Внешний API
 API = 'https://api.binance.com/'
 
-@app.route('/')
+#Главная страница
+@app.route('/') 
 def home():
     print(url_for('home'))
     return render_template('index.html')
     
-
-@app.route('/html/<smth>')
+#Возвращение endpoint1
+@app.route('/html/<smth>') 
 def html(smth):
     return render_template('page.html', page = url_for('html', smth = smth), endpoint = smth)
 
+#Возвращение endpoint2
 @app.route('/page')
 def info():
     return render_template('another.html', endpoint = url_for('info'))
 
-@app.route('/profile/<username>')
+#Возвращение endpoint3
+@app.route('/profile/<username>') 
 def login(username):
     return render_template('page.html', page = url_for('login', username = username), endpoint = username) 
 
-@app.route('/login/<int:id>')
+#Возвращение endpoint4
+@app.route('/login/<int:id>') 
 def user(id):
     return f"ID: <h1>{id}</h1>"
 
+#Для работы со swagger'ом
+@app.route('/cuerency/<type>')
+@swag_from('./docs/index.yml')
+def cuerency(type):
+    cuer = {'usd':['64', '65'],
+            'eur':['70', '71']}
+    if type == 'all':
+        result = cuer
+    else:
+        result = cuer.get(type)
 
+    return jsonify(result)
+
+#Вывод доступных публичных команд для API запроса
 @app.route('/binance', methods = ['POST', 'GET'])
 def api_requests():
     return render_template('binance.html', endpoint = f'Список доступных команд: {", ".join(methods)}') 
     
-
+#Вывод результата API запроса в виде json
 @app.route('/binance_api', methods = ['POST', 'GET'])  
 def binance_api():
     req = request.form['api_req']  
@@ -66,6 +102,7 @@ def binance_api():
     response = requests.get(API + methods[req]['url'])
     return response.json()
 
+#Класс для методов get, put 
 class ApiMethods(Resource):
     def get(self, todo_id):
         return {todo_id: todos[todo_id]}
@@ -74,6 +111,7 @@ class ApiMethods(Resource):
         todos[todo_id] = request.form['data']
         return {todo_id: todos[todo_id]}
 
+#Добавление ресурса к API 
 api.add_resource(ApiMethods, '/<string:todo_id>')
 
 if __name__ == '__main__':
