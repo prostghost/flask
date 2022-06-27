8278598f44f6ec0e20fc3be7d69e63af844d82be
prostghost <glyantsev27@gmail.com>
2022-06-27 22:36:26 +0300

    final

--- kekApp/mini-flask.py
+++ kekApp/mini-flask.py
@@ -1,12 +1,39 @@
+from msilib import Table
 from re import template
 from urllib import response
+from click import echo
 from flask import Flask, jsonify, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 import requests
-from flasgger import Swagger, swag_from
+from flasgger import Swagger, swag_from, validate
 from flask_restful import Api, Resource
+import sqlalchemy as sa
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/api_requests.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+db = SQLAlchemy(app)
+
+
+class Requests(db.Model):
+    __tablename__ = 'requsets'
+    __tableargs__ = {
+        'comment': 'Запросы'
+    }
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+db.create_all()
 
 #template для swagger'a
 template = {
@@ -40,13 +67,6 @@ methods = {
             'tickerBookTicker': {'url': 'api/v3/ticker/bookTicker', 'method': 'GET', 'private': False},
             }
 
-#Библиотека для get запросов
-todos = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
-
 #Внешний API
 API = 'https://api.binance.com/'
 
@@ -67,9 +87,35 @@ def info():
     return render_template('another.html', endpoint = url_for('info'))
 
 #Возвращение endpoint3
-@app.route('/profile/<username>') 
+@app.route('/profile/<username>', methods=['GET'])
+@swag_from('./docs/loginGET.yml', methods=['GET']) 
 def login(username):
     return render_template('page.html', page = url_for('login', username = username), endpoint = username) 
+
+import json
+
+@app.route('/profile')
+@app.route('/profile', methods=['POST'])
+@swag_from('./docs/loginPOST.yml', methods=['POST']) 
+def profile():
+    data = request.data
+    dataDict = json.loads(data)
+    username = dataDict['username']
+    email = dataDict['email']
+    password = dataDict['password']
+
+    user =  {
+        "username": username,
+        "email": email,
+        "password": password,
+        "userStatus": 0
+        }
+    
+    add_user = Requests(username = username, email = email, password = password)
+    db.session.add(add_user)
+    db.session.commit()
+
+    return jsonify(user)
 
 #Возвращение endpoint4
 @app.route('/login/<int:id>') 
@@ -102,18 +148,34 @@ def binance_api():
     response = requests.get(API + methods[req]['url'])
     return response.json()
 
+#Библиотека для get запросов
+todos = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
+
 #Класс для методов get, put 
 class ApiMethods(Resource):
+    @swag_from('./docs/get.yml')
     def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+        result = {todo_id: todos[todo_id]}
+        return jsonify (result)
 
+    @swag_from('./docs/post.yml')
+    def post(self, todo_id):
+        todos[todo_id] = {'task': 'new element'}
+        result = {todo_id: todos[todo_id]}
+        return jsonify (result)
+
+    @swag_from('./docs/put.yml', methods=['PUT'])
     def put(self, todo_id):
         todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
+        result = {todo_id: todos[todo_id]}
+        return jsonify (result)
 
 #Добавление ресурса к API 
-api.add_resource(ApiMethods, '/<string:todo_id>')
+api.add_resource(ApiMethods, '/<todo_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.run(debug=True)  
